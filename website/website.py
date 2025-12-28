49c848923804442e6673a64f3dded2523eac0768
Amr Amer <amribrahim.amer@gmail.com>
2025-12-28 14:32:50 +0100

    Update website.py

--- website/website.py
+++ website/website.py
@@ -14,7 +14,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("website/style.css")
 
 # Load the image from a local directory
 try:
