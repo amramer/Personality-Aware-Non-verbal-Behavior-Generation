65f14f479be877511029dae5ef52f1480bd182e7
Amr Amer <amribrahim.amer@gmail.com>
2025-12-28 14:40:41 +0100

    Update bio.py

--- website/bio.py
+++ website/bio.py
@@ -4,6 +4,6 @@ info = {
    "Sidebar Name": "<div id=\"sidebar_name\">Amr Amer</div>",
    "Sidebar About": "<div id=\"sidebar_about\">AI Engineer and Master’s Visual Computing graduate from Saarland University.</div>",
    "About": "Master’s Visual Computing graduate from Saarland University.",
-   "Photo": "Images/new_profile_photo.jpg",
+   "Photo": "website/Images/new_profile_photo.jpg",
    "Email": "amribrahim.amer@gmail.com"
 }
